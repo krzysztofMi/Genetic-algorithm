56bf6709b3e6b02494d59059825706b77f19729d
Krzosa Karol <krzosa.karol@gmail.com>
2021-05-29 19:22:12 +0200

    Recreating the pdf examples

---
+++ pythonDeap/oe_pdf_example1.py
@@ -0,0 +1,182 @@
+from deap import base
+from deap import creator
+from deap import tools
+import multiprocessing
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+import time
+import sys
+from pdf_code_formatted import SVCParameters, SVCParametersFitness, load_data
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+sizePopulation = 100
+probabilityMutation = 0.1
+probabilityCrossover = 0.8
+numberIteration=100
+
+
+# Copy paste we might create our own implementation of this algorithm
+def cx_arithmetic(ind1, ind2):
+    k = 0.25
+    ind1[0] = k * ind1[0] + (1 - k) * ind2[0]
+    ind1[1] = k * ind1[1] + (1 - k) * ind2[1]
+    ind2[0] = (1 - k) * ind1[0] + k * ind2[0]
+    ind2[1] = (1 - k) * ind1[1] + k * ind2[1]
+    return ind1, ind2
+
+
+# Copy paste we might create our own implementation of this algorithm
+def cx_heuristic(ind1, ind2):
+    if ind1[0]>ind1[1] and ind2[0]>ind2[1]:
+        k = random.uniform(0,1)
+        ind1[0] = k*(ind2[0]-ind1[0]) + ind1[0]
+        ind1[1] = k * (ind2[1] - ind1[1]) + ind1[1]
+        return ind1
+    else:
+        return None
+
+
+# It is in function for purpose to clean toolbox after each algorithm pass.
+def getToolbox(pool=None):
+    df, y, numberOfAtributtes = load_data()
+    toolbox = base.Toolbox()
+    if __name__ == "__main__" and pool is not None:
+        toolbox.register("map", pool.map) 
+    toolbox.register('individual',SVCParameters, numberOfAtributtes, creator.Individual) 
+    toolbox.register("evaluate", SVCParametersFitness,y,df,numberOfAtributtes)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("select", tools.selTournament, tournsize=5)
+    toolbox.register("mutate", tools.mutGaussian, mu=5, sigma=10, indpb=1)
+    toolbox.register("mate", tools.cxTwoPoint)
+    return toolbox
+
+
+def algorithm(toolbox, printBest = False):
+    pop = toolbox.population(n=sizePopulation)
+    fitnesses = list(toolbox.map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+    bestValues = []
+    means = []
+    stds = []
+    g = 0
+    numberElitism = 1
+
+    start = time.time()
+    while g < numberIteration:
+        g = g + 1
+        # print("-- Generation %i --" % g)
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+        listElitism = []
+        for x in range(0, numberElitism):
+            listElitism.append(tools.selBest(pop, 1)[0])
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            # cross two individuals with probability CXPB
+            if random.random() < probabilityCrossover:
+                toolbox.mate(child1, child2)
+                # fitness values of the children
+                # must be recalculated later
+                del child1.fitness.values
+                del child2.fitness.values
+        for mutant in offspring:
+        # mutate an individual with probability MUTPB
+            if random.random() < probabilityMutation:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        # print(" Evaluated %i individuals" % len(invalid_ind))
+        pop[:] = offspring + listElitism
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x * x for x in fits)
+        std = abs(sum2 / length - mean ** 2) ** 0.5
+        bestValues.append(min(fits))
+        means.append(mean)
+        stds.append(std)
+        # print(" Min %s" % min(fits))
+        # print(" Max %s" % max(fits))
+        # print(" Avg %s" % mean)
+        # print(" Std %s" % std)
+        best_ind = tools.selBest(pop, 1)[0]
+        # print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+    #
+    end = time.time()
+    duration = end-start
+    if(printBest):
+        print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+        print("Algorithm time %s" % (duration))
+    #print("-- End of (successful) evolution --")
+    return bestValues, means, stds, duration
+
+def plot_data(i, title, y_label, data):
+    x = np.arange(0, numberIteration, 1)
+    plt.subplot(3, 1, i)
+    plt.plot(x, data)
+    plt.title(title)
+    plt.ylabel(y_label)
+
+def make_plots(name, toolbox):
+    bestValues, means, stds, _ = algorithm(toolbox, True)
+    plot_data(1, "Best values in epochs", "best value", bestValues)
+    plot_data(2, "Means in epochs", "mean", means)
+    plot_data(3, "Standard deviations in epochs", "std", stds)
+    plt.xlabel("epoch")
+    plt.tight_layout()
+    plt.savefig(name + '_plot.png')
+    plt.clf()
+    
+
+def make_test(name, testSize, toolbox):
+    f = open(name + ".txt", "a")
+
+    minims = []
+    firstGen = []
+    times = []
+    for i in range(testSize):
+        bestValues, _, _, duration = algorithm(toolbox)
+        minims.append(np.min(bestValues))
+        firstGen.append(bestValues.index(np.min(bestValues)))
+        times.append(duration)
+        print(f"{name}:{i + 1}/{testSize}")
+    f.write(name)
+    f.write("\nNajlepsze znalezione rozwiazanie ")
+    f.write(str(np.min(minims)))
+    f.write("\nWartosc srednia najlepszych rozwiazan" )
+    f.write(str(np.mean(minims)))
+    f.write("\nOdchylenie standardowe najlepszych rozwiazan " )
+    f.write(str(np.std(minims)))
+    f.write("\nGeneracja, w ktorej najszybciej znaleziono optymalne rozwiazanie ")
+    f.write(str(np.min(firstGen)))
+    f.write("\nNajlepsze rozwiazanie znalezione najszybciej ")
+    f.write(str(minims[np.argmin(firstGen)]))
+    f.write("\nsrednia generacji, w ktorych po raz pierwszy znaleziono najlepsze rozwiazanie ")
+    f.write(str(np.mean(firstGen)))
+    f.write("\nOdchylenie standardowe generacji, w ktorych po raz pierwszy znaleziono najlepsze rozwiazanie ")
+    f.write(str(np.std(firstGen)))
+    f.write("\nsredni czas dzialania algorytmu [ms] ")
+    f.write(str(np.mean(times)) + "ms")
+    f.write("\nOdchylenie standardowe czasow dzialania algorytmu [ms] ")
+    f.write(str(np.std(times)) + "ms")
+    f.close()
+
+
+toolbox = getToolbox(None)
+make_plots("To co było w pdf", toolbox)
+toolbox = getToolbox(None)
+make_test("To co było w pdf", 50, toolbox)
+
+
+    
--- pythonDeap/pdf_code_formatted.py
+++ pythonDeap/pdf_code_formatted.py
@@ -3,23 +3,29 @@ from sklearn import model_selection
 from sklearn.preprocessing import MinMaxScaler 
 from sklearn.svm import SVC 
 
-pd.set_option('display.max_columns', None) 
-df=pd.read_csv("data.csv",sep=',') 
+def load_data():
+  pd.set_option('display.max_columns', None) 
+  df=pd.read_csv("data.csv",sep=',') 
 
-y=df['Status']
-df.drop('Status',axis=1,inplace=True)
-df.drop('ID',axis=1,inplace=True)
-df.drop('Recording',axis=1,inplace=True)
+  y=df['Status']
+  df.drop('Status',axis=1,inplace=True)
+  df.drop('ID',axis=1,inplace=True)
+  df.drop('Recording',axis=1,inplace=True)
 
-numberOfAtributtes=len(df.columns) 
-print(numberOfAtributtes)
+  numberOfAtributtes=len(df.columns) 
+  print(numberOfAtributtes)
 
 
-mms = MinMaxScaler() 
-df_norm = mms.fit_transform(df) 
-clf = SVC() 
-scores = model_selection.cross_val_score(clf, df_norm, y, cv=5, scoring='accuracy', n_jobs=-1) 
-print(scores.mean())
+  mms = MinMaxScaler() 
+  df_norm = mms.fit_transform(df) 
+  clf = SVC() 
+  scores = model_selection.cross_val_score(clf, df_norm, y, cv=5, scoring='accuracy', n_jobs=-1) 
+  print(scores.mean())
+  return df, y, len(df.columns)
+
+
+if __name__ == "__main__":
+  df, y, scores = load_data()
 
 import random 
 def SVCParameters(numberFeatures,icls):
@@ -31,7 +37,7 @@ def SVCParameters(numberFeatures,icls):
   k = random.uniform(0.1, 100)
   genome.append(k) 
   #degree
-  genome.append(random.int(0.1,5)) 
+  genome.append(random.uniform(0.1,5)) 
   #gamma 
   gamma = random.uniform(0.001,5)     
   genome.append(gamma) 
@@ -40,26 +46,25 @@ def SVCParameters(numberFeatures,icls):
   genome.append(coeff) 
   return icls(genome)
 
-import math from sklearn 
-import metrics 
+from sklearn import metrics 
+from sklearn.model_selection import StratifiedKFold
 def SVCParametersFitness(y,df,numberOfAtributtes,individual):     
-  split=5 cv = StratifiedKFold(n_splits=split)     
+  split=5 
+  cv = StratifiedKFold(n_splits=split)     
   mms = MinMaxScaler()   
   df_norm = mms.fit_transform(df)     
   estimator = SVC(kernel=individual[0],C=individual[1],degree=individual[2],gamma=individual[3],coef0=individual[4],random_state=101)     
   resultSum = 0 
   for train, test in cv.split(df_norm, y):
-    
-  estimator.fit(df_norm[train], y[train])        
-  predicted = estimator.predict(df_norm[test])
-  
-  expected = y[test]        
-  tn, fp, fn, tp = metrics.confusion_matrix(expected, predicted).ravel()
-  result = (tp + tn) / (tp + fp + tn + fn) 
+    estimator.fit(df_norm[train], y[train])        
+    predicted = estimator.predict(df_norm[test])
+    expected = y[test]        
+    tn, fp, fn, tp = metrics.confusion_matrix(expected, predicted).ravel()
+    result = (tp + tn) / (tp + fp + tn + fn) 
   #w oparciu o macierze pomyłek https://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/ 
-  resultSum = resultSum + result
+    resultSum = resultSum + result
   #zbieramy wyniki z poszczególnych etapów walidacji krzyżowej 
-  return resultSum / split
+  return resultSum / split,
 
 def mutationSVC(individual):
   numberParamer= random.randint(0,len(individual)-1) 
@@ -69,21 +74,21 @@ def mutationSVC(individual):
     individual[0]=listKernel[random.randint(0, 3)] 
   elif numberParamer==1:  
     k = random.uniform(0.1,100)
-      individual[1]=k 
+    individual[1]=k 
   elif numberParamer == 2: 
-      #degree 
-      individual[2]=random.uniform(0.1, 5) 
+    #degree 
+    individual[2]=random.uniform(0.1, 5) 
   elif numberParamer == 3: 
-      #gamma 
-      gamma = random.uniform(0.01, 5)
-      individual[3]=gamma 
+    #gamma 
+    gamma = random.uniform(0.01, 5)
+    individual[3]=gamma 
   elif numberParamer ==4: 
-      # coeff 
-      coeff = random.uniform(0.1, 20)
-      individual[2] = coeff
+    # coeff 
+    coeff = random.uniform(0.1, 20)
+    individual[2] = coeff
 
-toolbox.register('individual',SVCParameters, numberOfAtributtes, creator.Individual) 
-toolbox.register("evaluate", SVCParametersFitness,y,df,numberOfAtributtes)
+# toolbox.register('individual',SVCParameters, numberOfAtributtes, creator.Individual) 
+# toolbox.register("evaluate", SVCParametersFitness,y,df,numberOfAtributtes)
 
 
 def SVCParametersFeatures(numberFeatures,icls):
@@ -127,7 +132,7 @@ def SVCParametersFeatureFitness(y,df,numberOfAtributtes,individual):
     tn, fp, fn, tp = metrics.confusion_matrix(expected, predicted).ravel()
     result = (tp + tn) / (tp + fp + tn + fn) #w oparciu o macierze pomyłek https://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/ 
     resultSum = resultSum + result #zbieramy wyniki z poszczególnych etapów walidacji krzyżowej 
-  return resultSum / split
+  return resultSum / split,
 
 
 def mutationSVC(individual):
@@ -135,11 +140,11 @@ def mutationSVC(individual):
   if numberParamer==0: 
     # kernel 
     listKernel = ["linear", "rbf", "poly", "sigmoid"]
-      individual[0]=listKernel[random.randint(0, 3)] 
+    individual[0]=listKernel[random.randint(0, 3)] 
   elif numberParamer==1: 
     #C 
     k = random.uniform(0.1,100)
-      individual[1]=k 
+    individual[1]=k 
   elif numberParamer == 2: 
     #degree 
     individual[2]=random.uniform(0.1, 5) 
